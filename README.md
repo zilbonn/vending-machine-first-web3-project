5274991b85275cf03e65633901f9422ea8008c9c
JacobDiamond <thehackingmarket@protonmail.com>
2022-11-13 10:53:15 -0500

    first commit
    first commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# Vendeing machine - web3 project
+![](/images/solidity.png)
+
+### This project have been made with the help of this video
+
+[Smart Contract Tutorial - Vending Machine Smart Contract in Solidity](https://www.youtube.com/watch?v=wWAkR7CUwe4&t=0s&ab_channel=BlockExplorer)
---
+++ contracts/Vending_machine.sol
@@ -0,0 +1,30 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.11;
+
+contract VendingMachine {
+    address public owner;
+    mapping (address => uint) public donutBalances;
+
+    constructor(){
+        owner = msg.sender;
+        donutBalances[address(this)] = 100;
+    }
+
+    function getVendingMachineBalance() public view returns (uint) {
+        return donutBalances[address(this)];
+    }
+
+    function restock(uint amount) public {
+        require(msg.sender == owner, "Only the owner can restock the machine.");
+        donutBalances[address(this)] += amount;
+    }
+
+    function purchaes(uint amount) public payable {
+        require(msg.value  >= amount * 2 ether, "You must pay at least 2 ether per donut");
+        require(donutBalances[address(this)] >= amount, "Not enough donuts in stock to fulfill purchase request");
+        donutBalances[address(this)] -= amount;
+        donutBalances[msg.sender] += amount; 
+
+    }
+
+}
